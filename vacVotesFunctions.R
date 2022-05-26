be6aba477917d30ed82da99bada473570f916616
dviraran <dviraran@technion.ac.il>
2022-05-26 13:52:24 +0300

    first commit

---
+++ vacVotesFunctions.R
@@ -0,0 +1,166 @@
+
+readVaccination = function(file,date.vac2,date.vac3) {
+  
+  v = read.csv(file)
+  
+  A = v$date==date.vac2
+  B = v$date==date.vac3
+  vac = data.frame(town_code=v$town_code[A],town=v$town[A],agas_code=v$agas_code[A])
+  
+  vac$dose2=as.numeric(v$accumulated_vaccination_second_dose[A])
+  vac$dose3=as.numeric(v$accumulated_vaccination_third_dose[B])
+  vac$recovered2=as.numeric(v$accumulated_recoveries[A])
+  vac$recovered3=as.numeric(v$accumulated_recoveries[B])
+  vac$tests2=as.numeric(v$accumulated_diagnostic_tests[A])
+  vac$tests3=as.numeric(v$accumulated_diagnostic_tests[B])
+  vac$recovered.after = as.numeric(vac$recovered3)-as.numeric(vac$recovered2)
+  vac$tests.after = as.numeric(vac$tests3)-as.numeric(vac$tests2)
+  
+  agas = str_pad(as.numeric(trimws(vac$agas_code)),4,pad = "0")
+  agas[is.na(agas)]='0000'
+  vac$code = paste0(vac$town_code,agas)
+  vac
+}
+
+readSE = function(working.dir) {
+  se1 = openxlsx::read.xlsx(paste0(working.dir,'data/lamas-t12.xlsx'))
+  se2 = openxlsx::read.xlsx(paste0(working.dir,'data/lamas-t01.xlsx'))
+  se3 = openxlsx::read.xlsx(paste0(working.dir,'data/lamas-t07.xlsx'))
+  
+  se = rbind(se1,se2[se2$TOWN_CODE %in% setdiff(se2$TOWN_CODE,se1$TOWN_CODE),])
+  se = rbind.fill(se,se3[se3$TOWN_CODE %in% setdiff(se3$TOWN_CODE,se$TOWN_CODE),])
+  
+  agas = str_pad(as.numeric(trimws(se$AGAS_CODE)),4,pad = "0")
+  agas[is.na(agas)]='0000'
+  se$code = paste0(se$TOWN_CODE,agas)
+  se
+}
+
+readVotes = function(working.dir,election=2020) {
+  
+  if (election==2021) {
+    print(election)
+    votes = openxlsx::read.xlsx(paste0(working.dir,'data/bechirot2021.xlsx'))
+    votes$kalfi.code = paste0((votes$town_code),'.',unlist(lapply(as.character(votes$Kalfi_id),FUN=function(x) strsplit(x,'[.]')[[1]][1])))
+    
+    map.votes.stat = openxlsx::read.xlsx(paste0(working.dir,'data/kalpi_january2021_stat2011_6010ד.xlsx'))
+    kalfi.map = openxlsx::read.xlsx(paste0(working.dir,'data/kalfi_2021_transform.xlsx'))
+    for (i in 1:ncol(kalfi.map)) {
+      id = map.votes.stat$town_code==kalfi.map$town_code[i] & map.votes.stat$kalfi_id==kalfi.map$orig_id[i]
+      if(sum(id)==1) {
+        map.votes.stat$kalfi_id[id] =  kalfi.map$new_id[i]
+      }
+      if (sum(id)==0) {
+        print(i)
+      }
+    }
+    map.votes.stat$kalfi.code = paste0(map.votes.stat$town_code,'.',map.votes.stat$kalfi_id)
+  } else if (election==2020) {
+    print(election)
+
+    votes = openxlsx::read.xlsx(paste0(working.dir,'data/bechirot2020.xlsx'))
+    votes$kalfi.code = paste0((votes$town_code),'.',unlist(lapply(as.character(votes$Kalfi_id),FUN=function(x) strsplit(x,'[.]')[[1]][1])))
+    map.votes.stat = openxlsx::read.xlsx(paste0(working.dir,'data/kalpi_March2020_stat2011.xlsx'))
+    map.votes.stat$kalfi.code = paste0(map.votes.stat$semel_yishuv,'.',map.votes.stat$kalpi2019)
+    map.votes.stat$code = map.votes.stat$yishuv_stat2011
+  }
+  
+  votes = merge(votes,map.votes.stat,by='kalfi.code')
+  votes
+}
+
+assignSector = function(m,election) {
+  
+  cities = read.csv(paste0(working.dir,'data/cities.csv'))
+  
+  arabs = unique(cities$city[cities$דסק ==  'דסק ערבי'])
+  ultraorthodox = unique(cities$city[cities$דסק ==  'דסק חרדי'])
+  
+  m$sector = 'General'
+  m$sector[m$town %in% arabs] = 'Arab'
+  m$sector[m$town %in% ultraorthodox] = 'Haredi'
+  
+  if (election==2021) {
+    arabs = m$Raam+m$Aravit>0.25
+    
+  } else if (election==2020) {
+    arabs = m$ReshimaAravit>0.25
+  }
+  
+  ultraorthodox = m$YahadutHatora+m$Shas>0.25
+  
+  
+  m$sector[arabs] = 'Arab'
+  m$sector[ultraorthodox] = 'Haredi'
+  
+  mahoz = cities$מחוז
+  mahoz = mapvalues(mahoz,from = unique(mahoz), to = c('Missing','Jerusalem','North','South','Haifa','Ashkelon','Merkaz','Tel Aviv','Unkown','empty'))
+  m$region = 'Missing'
+  umahoz = unique(mahoz)
+  for (i in 1:length(umahoz)) {
+    
+    m$region[m$town %in% cities$city[mahoz==umahoz[i]]] = umahoz[i]
+    
+  }
+  
+  m
+}
+
+toCityLevel = function(vac,votes) {
+  
+  socioeconomic = read.csv(paste0(working.dir,'data/socioeconomic.cities.csv'))
+  
+  
+  vac.city = ddply(vac,"town",numcolwise(sum))
+  
+  mcity = merge(vac.city,socioeconomic,by.x='town',by.y='city')
+  
+  votes2 = votes[,c('town_code','BZB','N',intersect(parties,colnames(votes)))]
+  votes2 = ddply(votes2,"town_code",numcolwise(sum))
+  votes2[,intersect(parties,colnames(votes))] = votes2[,intersect(parties,colnames(votes))]/votes2$BZB
+  
+  mcity = merge(mcity,votes2,by='town_code')
+  
+  
+  mcity$dose2.perc = as.numeric(mcity$dose2)/mcity$BZB
+  mcity$dose3.perc = as.numeric(mcity$dose3)/mcity$BZB
+  
+  mcity$diff = 100*(mcity$dose2.perc-mcity$dose3.perc)
+  mcity$avoid = 100*(1-mcity$dose3.perc/mcity$dose2.perc)
+  mcity$dose3.dose2 = (100-mcity$avoid)/100
+  
+  mcity
+}
+
+getAreasAges = function(working.dir) {
+  
+  ages = openxlsx::read.xlsx(paste0(working.dir,'data/deciles.xlsx'),1)
+  colnames(ages) = c('town_ages','town_code_ages','code','Nsize','p10','p20','p30','p40','p50','p60','p70','p80','p90')
+  
+  for (i in 1:nrow(ages)) {
+    if (ages[i,13]>60) {
+      j = min(which(ages[i,5:13]>50))
+      percent = (ages[i,(j+5)]-ages[i,(j+5)-1])/10
+      ages$age60[i] = min(100,100-(j*10 + (60-ages[i,(j+5)-1])/percent))
+    } else if (ages[i,13]>50) {
+      ages$age60[i] = ages[i,13]-50
+    } else {
+      ages$age60[i] = 0
+    }
+  }
+  
+  for (i in 1:nrow(ages)) {
+    if (ages[i,13]>20) {
+      j = min(which(ages[i,5:13]>20))
+      percent = (ages[i,(j+5)]-ages[i,(j+5)-1])/10
+      ages$age20[i] = min(100,100-(j*10 + (20-ages[i,(j+5)-1])/percent))
+    } else if (ages[i,13]>20) {
+      ages$age20[i] = ages[i,13]-20
+    } else {
+      ages$age20[i] = 0
+    }
+  }
+  ages$age20 = ages$age20-ages$age60
+  
+  ages
+}
---
+++ vax3.R
@@ -0,0 +1,193 @@
+### read libraries. TODO: check what is really required
+
+library(stargazer)
+library(forestplot)
+library(ggplot2)
+library(ggpubr)
+library(reshape2)
+library(plyr)
+library(dplyr)
+library(stringr)
+library(ggrepel)
+library(ggeffects)
+library(cowplot)
+library(robust)
+library(lme4)
+library(afex) 
+library(sjmisc)
+
+####
+eps = 0.0001
+working.dir ='~/Documents/covid_analyses/vacVotes/'
+source(paste0(working.dir,'vacVotesFunctions.R'))
+
+####
+
+parties.all = c("Avoda", "Yamina","ZionutDatit","KaholLavan" ,  
+                "IsraelBeitenu", "Likud",  "Meretz", "YeshAtid", "Shas" ,"TikvaHadasha","Raam","YahadutHatora",'Aravit')
+parties.group.all = c('Left','Center','Right','Center','Center','Right','Left','Left','Haredi','Center','Arab','Haredi','Arab')
+
+###
+
+election=2021
+
+votes = readVotes(working.dir,election=election)
+votes = votes[,c('code','BZB','N',parties.all)]
+votes = ddply(votes,"code",numcolwise(sum))
+votes[,intersect(parties.all,colnames(votes))] = votes[,intersect(parties.all,colnames(votes))]/votes$BZB
+
+vac = read.csv(paste0(working.dir,'data/geographic-sum-per-day-ver_00714_small.csv'))
+agas = str_pad(as.numeric(trimws(vac$agas_code)),4,pad = "0")
+agas[is.na(agas)]='0000'
+vac$code = paste0(vac$town_code,agas)
+vac$date = as.Date(vac$date)
+
+## choose rows every 30 days from dose 1, and every 30 days from dose 3
+vac = subset(vac,vac$date %in% c(seq(from=as.Date('2020-12-21'),to=as.Date('2021-07-30'),by='30 day'),
+                                 seq(from=as.Date('2021-07-31'),to=as.Date('2021-11-30'),by='30 day')))
+
+socioeconomic = readSE(working.dir)
+ages = getAreasAges(working.dir)
+
+df = merge(vac,votes,by='code')
+df = merge(df,socioeconomic,by='code')
+df = merge(df,ages,by='code')
+
+#if deidentified (<10) make it 5
+df$accumulated_vaccination_first_dose[is.na(as.numeric(df$accumulated_vaccination_first_dose))] = 5
+df$accumulated_vaccination_second_dose[is.na(as.numeric(df$accumulated_vaccination_second_dose))] = 5
+df$accumulated_vaccination_third_dose[is.na(as.numeric(df$accumulated_vaccination_third_dose))] = 5
+df$accumulated_cases[is.na(as.numeric(df$accumulated_vaccination_third_dose))] = 5
+df$accumulated_recoveries[is.na(as.numeric(df$accumulated_recoveries))] = 5
+
+df$time = as.numeric(as.Date(df$date))
+df$time = df$time-min(df$time)
+
+# define vote groups
+df$ProBibi = df$Likud+df$Shas+df$YahadutHatora+df$ZionutDatit
+df$AntiBibi = df$Meretz+df$YeshAtid+df$Avoda+df$TikvaHadasha+df$IsraelBeitenu
+df$Other = 1-df$Likud-df$Shas-df$Avoda-df$Meretz-df$YeshAtid-df$TikvaHadasha-df$IsraelBeitenu-df$YahadutHatora-df$ZionutDatit
+df$OtherNoVote = 1-rowSums(df[,parties.all])
+
+## log relative to ref of votes
+df$AntiBibiL = log((df$AntiBibi+eps)/(df$ProBibi+eps))
+df$OtherL = log((df$Other+0.0001)/(df$ProBibi+eps))
+
+df$reference = df$Likud
+df$ShasL = log((df$Shas+eps)/(df$reference+eps))
+df$YahadutHatoraL = log((df$YahadutHatora+eps)/(df$reference+eps))
+df$ZionutDatitL = log((df$ZionutDatit+eps)/(df$reference+eps))
+df$KaholLavanL = log((df$KaholLavan+eps)/(df$reference+eps))
+df$MeretzL = log((df$Meretz+eps)/(df$reference+eps))
+df$YeshAtidL = log((df$YeshAtid+eps)/(df$reference+eps))
+df$AvodaL = log((df$Avoda+eps)/(df$reference+eps))
+df$YaminaL = log((df$Yamina+eps)/(df$reference+eps))
+df$TikvaHadashaL = log((df$TikvaHadasha+eps)/(df$reference+eps))
+df$IsraelBeitenuL = log((df$IsraelBeitenu+eps)/(df$reference+eps))
+df$RaamL = log((df$Raam+eps)/(df$reference+eps))
+df$AravitL = log((df$Aravit+eps)/(df$reference+eps))
+df$OtherNoVoteL = log((df$OtherNoVote+eps)/(df$reference+eps))
+df$LikudL = log((df$Likud+eps)/(df$reference+eps))
+
+
+# change - date after coalition change
+df$change = as.numeric(as.Date(df$date,origin='1899-12-21')>=as.Date('2021-06-13'))
+
+df$code = factor(df$code)
+df$AGE_SV = as.numeric(df$AGE_SV)
+df$accumulated_cases = as.numeric(df$accumulated_cases)
+
+df$vax1 = as.numeric(df$accumulated_vaccination_first_dose)
+df$vax3 = as.numeric(df$accumulated_vaccination_third_dose)
+
+df$vax_potential1 = df$Nsize
+vax_potential3 = as.numeric(df$accumulated_vaccination_first_dose[df$time==150])
+names(vax_potential3) = df$code[df$time==150]
+df$vax_potential3 = vax_potential3[df$code]
+
+df$weights = df$Nsize/max(df$Nsize)
+
+createDataTimeFromStrat = function(df,n.months) {
+  out1 = subset(df,df$time==n.months*30)
+  out1$vax = out1$vax1
+  out1$vax_potential = out1$vax_potential1
+  
+  out2 = subset(df,df$time==222+n.months*30)
+  out2$vax = out2$vax3
+  out2$vax_potential = out2$vax_potential3
+  
+  
+  out = rbind(out1,out2)
+  out
+}
+
+df1 = createDataTimeFromStrat(df, n.months=1)
+df2 = createDataTimeFromStrat(df, n.months=2)
+df3 = createDataTimeFromStrat(df, n.months=3)
+
+#### create models (not working now, need to choose how to do them)
+
+model1 = glmer(vax ~ SE_INDEX + age20 + age60 + change + OtherL + OtherL:change + AntiBibiL + AntiBibiL:change + accumulated_cases + offset(log(vax_potential)) + (1|code), 
+               data=temp,family=poisson,
+               control=glmerControl(optCtrl=list(maxfun=2e5)))
+
+model1 = lmer(vax ~ SE_INDEX + age20 + age60 + change + OtherL + OtherL:change + AntiBibiL + AntiBibiL:change + accumulated_cases + vax_potential + (1|code), 
+              weights = weights, data=df1)
+
+model2 = lmer(vax ~ SE_INDEX + age20 + age60 + change + OtherL + OtherL:change + AntiBibiL + AntiBibiL:change + accumulated_cases + vax_potential + (1|code), 
+              weights = weights, data=df2)
+
+model3 = lmer(vax ~ SE_INDEX + age20 + age60 + change + OtherL + OtherL:change + AntiBibiL + AntiBibiL:change + accumulated_cases + vax_potential + (1|code), 
+              weights = weights, data=df3)
+
+sjPlot::tab_model(list(model1,model2,model3),digits=4,collapse.ci =T,  show.intercept = F,p.style='stars')
+
+sjPlot::plot_model(model1,show.values=TRUE, show.p=TRUE)
+
+sjPlot::plot_models(model3,model2,model1,show.values=TRUE, show.p=TRUE,grid = T)+geom_hline(yintercept = 0,linetype='dashed')
+
+
+###### model per party
+
+model3 = lmer(vax ~ 
+                SE_INDEX + 
+                age20 + age60 +
+                change + 
+                
+                ShasL + ShasL:change + 
+                YahadutHatoraL + YahadutHatoraL:change + 
+                ZionutDatitL + ZionutDatitL:change + 
+                KaholLavanL + KaholLavanL:change + 
+                MeretzL + MeretzL:change +
+                YeshAtidL + YeshAtidL:change + 
+                AvodaL + AvodaL:change + 
+                YaminaL + YaminaL:change + 
+                IsraelBeitenuL + IsraelBeitenuL:change + 
+                TikvaHadashaL + TikvaHadashaL:change +
+                RaamL + RaamL:change +
+                AravitL + AravitL:change +
+                OtherNoVoteL + OtherNoVoteL:change +
+                
+                accumulated_cases + 
+                vax_potential + 
+                (1|code), 
+              weights = weights, data=df3)
+
+sjPlot::plot_model(model3,show.values=TRUE, show.p=TRUE,
+                   terms = c('change:ShasL','change:YahadutHatoraL','change:ZionutDatitL',
+                             'change:YaminaL','change:TikvaHadashaL','change:IsraelBeitenuL',
+                             'change:KaholLavanL','change:YeshAtidL','change:AvodaL',
+                             'change:MeretzL','change:RaamL','change:AravitL'
+                   ))+
+  geom_hline(yintercept = 0,linetype='dashed')
+
+
+sjPlot::plot_models(model3,model2,model1,show.values=TRUE, show.p=TRUE,
+                    terms = c('change:Shas','change:YahadutHatora','change:ZionutDatit',
+                              'change:Yamina','change:TikvaHadasha','change:IsraelBeitenu',
+                              'change:KaholLavan','change:YeshAtid','change:Avoda',
+                              'change:Meretz','change:Raam','change:Aravit'
+                    ),
+                    axis.lim = c(-0.1,0.1))+
+  geom_hline(yintercept = 0,linetype='dashed')
+
